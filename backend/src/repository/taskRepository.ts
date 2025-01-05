9a3213ec12d9644363e40d3448db6f6778d78140
Enchan <enchant0130@gmail.com>
2025-01-05 16:18:02 +0900

    [Modify] interface of /task
    - タスクリスト取得APIで詳細を結果に含めないように変更

--- backend/src/repository/taskRepository.ts
+++ backend/src/repository/taskRepository.ts
@@ -22,34 +22,60 @@ export const deleteTask = (id: Task["id"]): Task | undefined => {
   return storedTask;
 };
 
-export const listTasks = (
+export type TaskListItem = Omit<Task, "description">;
+
+/**
+ * 条件をもとにタスクリストのソート関数を構築する
+ * @param sortBy ソート対象のキー
+ * @param order 昇順・降順
+ * @returns ソート関数
+ */
+const buildTaskListItemSortFunction = (
   sortBy: keyof Pick<Task, "id" | "limit" | "priority">,
   order: "asc" | "desc"
-): Task[] => {
-  if (sortBy === "id") {
-    return Array.from(tasks.values()).toSorted(compare<Task>(sortBy, order));
-  }
-
+): ((lhs: TaskListItem, rhs: TaskListItem) => number) => {
   if (sortBy === "limit") {
-    return Array.from(tasks.values()).toSorted(
-      // 同じ期限ならIDで比較する
-      compare<Task>(sortBy, order, compare<Task>("id", order))
+    // 同じ期限ならIDで比較する
+    return compare<TaskListItem>(
+      sortBy,
+      order,
+      compare<TaskListItem>("id", order)
     );
   }
 
-  const sortedTasks = Array.from(tasks.values()).toSorted((lhs, rhs) => {
-    const lpr = lhs.priority;
-    const rpr = rhs.priority;
-    if (lpr === rpr) {
-      // 同じ優先度ならIDで比較する
-      return compare<Task>("id", order)(lhs, rhs);
-    }
+  if (sortBy === "priority") {
+    return (lhs, rhs) => {
+      const lpr = lhs.priority;
+      const rpr = rhs.priority;
+      if (lpr === rpr) {
+        // 同じ優先度ならIDで比較する
+        return compare<TaskListItem>("id", order)(lhs, rhs);
+      }
 
-    // 優先度は数値に変換して比較する
-    const lprLevel = TaskPriorityLevelMap[lpr];
-    const rprLevel = TaskPriorityLevelMap[rpr];
-    const direction = order === "asc" ? 1 : -1;
-    return (lprLevel > rprLevel ? 1 : -1) * direction;
-  });
-  return sortedTasks;
+      // 優先度は数値に変換して比較する
+      const lprLevel = TaskPriorityLevelMap[lpr];
+      const rprLevel = TaskPriorityLevelMap[rpr];
+      const direction = order === "asc" ? 1 : -1;
+      return (lprLevel > rprLevel ? 1 : -1) * direction;
+    };
+  }
+
+  return compare<TaskListItem>(sortBy, order);
+};
+
+export const listTasks = (
+  sortBy: keyof Pick<Task, "id" | "limit" | "priority">,
+  order: "asc" | "desc"
+): TaskListItem[] => {
+  const taskListData: TaskListItem[] = Array.from(tasks.values()).map(
+    ({ id, title, limit, priority }) => ({
+      id,
+      title,
+      limit,
+      priority,
+    })
+  );
+
+  const sortFunction = buildTaskListItemSortFunction(sortBy, order);
+  return taskListData.toSorted(sortFunction);
 };
