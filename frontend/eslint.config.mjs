c05606425f1aeb58373d43dbe4558071500be0ed
Enchan <enchant0130@gmail.com>
2025-01-06 10:30:07 +0900

    [Modify] configure stylistic for frontend

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,10 +3,34 @@ import eslint from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
 import globals from "globals";
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
 
 export default tseslint.config(
     // .eslintignore
     { ignores: ['dist'] },
+
+    // configurations for stylistic
+    {
+        plugins: {
+            '@stylistic': stylistic,
+            '@stylistic/ts': stylisticTs,
+        },
+        rules: {
+            '@stylistic/indent': ['error', 2],
+            '@stylistic/max-len': [
+                'error',
+                {
+                    code: 80,
+                    ignoreUrls: true,
+                },
+            ],
+            '@stylistic/quotes': ['error', 'single'],
+            '@stylistic/semi': ['error', 'always'],
+            '@stylistic/space-infix-ops': 'error',
+            "@stylistic/ts/space-infix-ops": "error",
+        },
+    },
 
     // configurations for Vue
     // based on: https://eslint.vuejs.org/user-guide/#example-configuration-with-typescript-eslint-and-prettier
@@ -26,4 +50,11 @@ export default tseslint.config(
             pluginVue.configs['flat/recommended'],
         ],
     },
+    
+    {
+        rules: {
+            'no-console': 'warn',
+            eqeqeq: ['error', 'always'],
+        }
+    }
 );
