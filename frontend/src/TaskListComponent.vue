b69c381b4ae59131bdbea67752708dc8a1df6c98
enchan <enchant0130@gmail.com>
2025-01-25 18:12:46 +0900

    feat(be): #3 コンポーネント分割

---
+++ frontend/src/TaskListComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>Registered tasks</h2>
+  <ul>
+    <li
+      v-for="task in props.tasks"
+      :key="task.id"
+    >
+      <ul>
+        <li>title: {{ task.title }}</li>
+        <li>priority: {{ task.priority }}</li>
+        <li>due: {{ task.limit.toISOString() }}</li>
+        <li>
+          <button @click="emits('showDetail', task.id)">
+            show details
+          </button>
+        </li>
+        <li>
+          <button @click="emits('delete', task.id)">
+            delete
+          </button>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type { TaskListItem } from '@backend/repository/taskRepository'
+
+const props = defineProps<{ tasks: TaskListItem[] }>()
+const emits = defineEmits<{
+  (
+    operation: 'showDetail' | 'delete',
+    id: TaskListItem['id']
+  ): void
+}>()
+
+</script>
