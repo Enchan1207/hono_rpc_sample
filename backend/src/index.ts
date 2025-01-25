2809316431cfcd714a81919782857a376a731860
enchan <enchant0130@gmail.com>
2025-01-25 10:16:50 +0900

    feat(lb): show tasks to component, use CORS middleware for route /task

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import tasks from '@/routes/tasks';
 
 const app = new Hono();
+app.use('/task/*', cors());
 
 // eslint-disable-next-line unusedImport/no-unused-vars
 const routes = app.route('/task', tasks);
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <h2>Hono RPC sample</h2>
-  <div>under construction...</div>
+  <div>registered tasks</div>
+  <ul>
+    <li
+      v-for="task in tasks"
+      :key="task.id"
+    >
+      {{ task.id }} {{ task.title }} (due: {{ task.limit }}
+      priority: {{ task.priority }})
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import type { TaskListItem } from '@backend/repository/taskRepository';
 import { client } from './client';
 
+const tasks = ref<TaskListItem[]>([]);
+
 onMounted(async()=>{
-  const tasks = client.task.$get({query:{}});
-  console.log(tasks);
+  // TODO: 双方プリミティブ型でやりとりした方がよいのでは?
+  const tasksData = await (await client.task.$get({query:{}})).json();
+  tasks.value = tasksData.map(({id, title, limit, priority})=>({
+    id,
+    title,
+    priority,
+    limit: new Date(limit),
+  }));
 });
 
 </script>
