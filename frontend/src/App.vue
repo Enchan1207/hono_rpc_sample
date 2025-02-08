cf7b2def49d74748ef9bdcacfbebbfe1401fdd23
enchan <enchant0130@gmail.com>
2025-02-08 23:01:54 +0900

    feat: #12 configure basic dashboard-like ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,31 @@
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue'
+
+</script>
+
 <template>
-  <h1>Hono RPC sample</h1>
-  <RouterView />
+  <el-container class="root-container">
+    <el-header>
+      <h1>Hono RPC sample</h1>
+    </el-header>
+    <el-container>
+      <el-aside>
+        <Sidebar />
+      </el-aside>
+      <el-main>
+        <RouterView />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.root-container {
+  height: 100vh;
+}
+</style>
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template>
+  <div class="add-button-container">
+    <el-button
+      type="primary"
+      :icon="Edit"
+      size="large"
+      @click="router.push('/tasks/new')"
+    >
+      追加
+    </el-button>
+  </div>
+  <el-menu :router="true">
+    <el-menu-item index="/tasks">
+      <el-icon><el-icon-home-filled /></el-icon>
+      <span>ホーム</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style>
+.el-menu-item {
+  user-select: none;
+}
+
+.add-button-container{
+  padding: 10px 20px;
+}
+
+.add-button-container .el-button {
+  width: 100%;
+  height: 50px;
+  justify-content: left;
+}
+</style>
