27318150e053c70032c471d77fbc024827058495
enchan <enchant0130@gmail.com>
2025-02-09 22:04:56 +0900

    feat: #12 v-cardで書き直し

--- frontend/src/components/tasks/TaskList.vue
+++ frontend/src/components/tasks/TaskList.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { DeleteFilled } from '@element-plus/icons-vue'
-import dayjs from '@/logic/dayjs'
+import TaskListRow from './TaskListRow.vue'
 import type { TaskListItem } from '@/entities/task'
 
 const props = defineProps<{
@@ -20,79 +19,28 @@ const emits = defineEmits<{
 </script>
 
 <template>
-  <el-table
-    v-loading="isLoading"
-    v-infinite-scroll="()=>emits('next')"
-    :v-infinite-scroll-distance="10"
-    :infinite-scroll-disabled="isLoading || !hasNext"
-    :data="props.tasks"
-    :height="200"
-  >
-    <el-table-column
-      prop="title"
-      label="タイトル"
+  <ul class="task-list">
+    <li
+      v-for="task in props.tasks"
+      :key="task.id"
     >
-      <template #default="{row: task}">
-        <div class="title-cell">
-          <el-button
-            link
-            type="primary"
-            @click="emits('detail', task.id)"
-          >
-            {{ task.title }}
-          </el-button>
-          <el-button
-            class="remove-button"
-            type="danger"
-            :icon="DeleteFilled"
-            size="small"
-            circle
-            @click.stop="emits('remove', task.id)"
-          />
-        </div>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="priority"
-      label="優先度"
-    />
-    <el-table-column
-      prop="due"
-      label="期日"
-    >
-      <template #default="{row: task}">
-        <span :class="task.due.isBefore(dayjs()) ? 'over':''">
-          {{ task.due.fromNow() }}
-        </span>
-      </template>
-    </el-table-column>
-  </el-table>
+      <TaskListRow
+        :item="task"
+        @detail="emits('detail', task.id)"
+        @remove="emits('remove', task.id)"
+      />
+    </li>
+  </ul>
 </template>
 
 <style>
-.el-table__row {
-  user-select: none;
+.task-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 
-.remove-button {
-  opacity: 0;
-  pointer-events: none;
-  transition: all ease-in-out .1s;
-}
-
-.el-table__row:hover .remove-button{
-  opacity: 1;
-  pointer-events: all;
-}
-
-.el-table__row .over{
-  color: red;
-  font-weight: bold;
-}
-
-.title-cell {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.task-list > li {
+  margin-bottom: 10px;
 }
 </style>
---
+++ frontend/src/components/tasks/TaskListRow.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { DeleteFilled } from '@element-plus/icons-vue'
+import type { TaskListItem } from '@/entities/task'
+
+const props = defineProps<{ item: TaskListItem }>()
+const emits = defineEmits(['detail', 'remove'])
+</script>
+
+<template>
+  <el-card>
+    <el-row>
+      <el-col :span="22">
+        <el-button
+          type="primary"
+          link
+          @click="emits('detail')"
+        >
+          {{ props.item.title }}
+        </el-button>
+        <div>priority: {{ props.item.priority }}</div>
+        <div>due:{{ props.item.due.fromNow() }}</div>
+      </el-col>
+
+      <el-col :span="2">
+        <el-button
+          class="remove-button"
+          type="danger"
+          :icon="DeleteFilled"
+          circle
+          size="small"
+          @click="emits('remove')"
+        />
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<style>
+.remove-button {
+  pointer-events: none;
+  opacity: 0;
+  transition: opacity .1s ease-in-out;
+}
+
+.el-card:hover .remove-button {
+  pointer-events: all;
+  opacity: 1;
+}
+</style>
