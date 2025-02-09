15ddb462c401d70b11d5a4730dc6b5678b400b67
enchan <enchant0130@gmail.com>
2025-02-09 17:34:48 +0900

    feat: #12 element-plusのコンポーネントに移行

--- frontend/src/pages/tasks/index.vue
+++ frontend/src/pages/tasks/index.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <button @click="onClickOrder">
-      order: {{ order }}
-    </button>
+    <el-select v-model="key">
+      <el-option
+        v-for="[sortKey, label] in Object.entries(sortOpts)"
+        :key="sortKey"
+        :label="label"
+        :value="sortKey"
+      />
+    </el-select>
 
-    <button @click="onClickKey">
-      key: {{ key }}
-    </button>
-
-    <label for="item_per_page">Items per page:</label>
-    <select
-      id="item_per_page"
-      v-model="itemPerPage"
+    <el-button
+      :icon="order==='asc' ? SortUp : SortDown"
+      @click="onClickOrder"
     >
-      <template
-        v-for="count in itemCountSelections"
-        :key="count"
-      >
-        <option
-          :value="count"
-        >
-          {{ count }}
-        </option>
-      </template>
-    </select>
+      {{ orderLabel[order] }}
+    </el-button>
+
+    <button
+      v-if="hasNext"
+      :disabled="isLoading"
+      @click="next"
+    >
+      load more
+    </button>
+    <template v-else>
+      -- last item --
+    </template>
   </div>
 
   <template v-if="isLoading">
@@ -40,34 +42,38 @@
       @detail="onClickDetail"
       @remove="remove"
     />
-    <button
-      v-if="hasNext"
-      :disabled="isLoading"
-      @click="next"
-    >
-      load more
-    </button>
-    <template v-else>
-      -- last item --
-    </template>
   </template>
 </template>
 
 <script setup lang="ts">
+import { SortDown, SortUp } from '@element-plus/icons-vue'
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import type { Ref } from 'vue'
 import type { TaskListItem } from '@/entities/task'
 import TaskList from '@/components/tasks/TaskList.vue'
 import { useTaskList } from '@/composables/useTaskList'
 
 const router = useRouter()
 
-const key = ref<'id' | 'due' | 'priority'>('id')
-const order = ref <'asc' | 'desc'>('desc')
+// 並べ替え項目
+type SortKeys = 'id' | 'due' | 'priority'
+const sortOpts: Record<SortKeys, string> = {
+  id: '作成日',
+  due: '期日',
+  priority: '優先度',
+}
+const key = ref<SortKeys>('id')
 
-const itemCountSelections = [5, 10, 20, 50] as const
-const itemPerPage: Ref<(typeof itemCountSelections)[number]> = ref(20)
+// 並べ替え順序
+type Orders = 'asc' | 'desc'
+const orderLabel: Record<Orders, string> = {
+  asc: '昇順',
+  desc: '降順',
+}
+const order = ref <Orders>('desc')
+
+/** @deprecated 無限スクロール実装するならUIとして項目用意する必要ないはず */
+const itemPerPage = ref(20)
 
 const {
   tasks,
@@ -94,21 +100,5 @@ const onClickDetail = (id: TaskListItem['id']) => {
 const onClickOrder = () => {
   const currentOrder = order.value
   order.value = currentOrder === 'asc' ? 'desc' : 'asc'
-}
-
-const onClickKey = () => {
-  switch (key.value) {
-    case 'id':
-      key.value = 'due'
-      break
-
-    case 'due':
-      key.value = 'priority'
-      break
-
-    case 'priority':
-      key.value = 'id'
-      break
-  }
 }
 </script>
