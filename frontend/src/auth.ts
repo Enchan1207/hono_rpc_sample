4107143b6c5eedfed7953d6e012aca93ca655b87
enchan <enchant0130@gmail.com>
2025-03-04 23:17:18 +0900

    fix: #44 auth0ドメインとクライアントIDに関してはVITEで公開する必要があった

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -2,7 +2,7 @@ import { createAuth0 } from '@auth0/auth0-vue'
 
 const env = import.meta.env
 export const auth = createAuth0({
-  domain: env.AUTH_DOMAIN,
-  clientId: env.AUTH_CLIENT_ID,
+  domain: env.VITE_AUTH_DOMAIN,
+  clientId: env.VITE_AUTH_CLIENT_ID,
   authorizationParams: { redirect_uri: window.location.origin },
 })
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,8 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string
-  readonly AUTH_DOMAIN: string
-  readonly AUTH_CLIENT_ID: string
+  readonly VITE_AUTH_DOMAIN: string
+  readonly VITE_AUTH_CLIENT_ID: string
 }
 
 interface ImportMeta { readonly env: ImportMetaEnv }
