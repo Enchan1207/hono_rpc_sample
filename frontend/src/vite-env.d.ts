7df9ebba1dbd2e0e82d046134dc459f46b4da425
enchan <enchant0130@gmail.com>
2025-03-05 23:17:24 +0900

    chore: #44 audienceを設定, トークンプロバイダを設定

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string
   readonly VITE_AUTH_DOMAIN: string
   readonly VITE_AUTH_CLIENT_ID: string
+  readonly VITE_AUTH_AUDIENCE: string
 }
 
 interface ImportMeta { readonly env: ImportMetaEnv }
