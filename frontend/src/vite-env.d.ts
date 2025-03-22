2d34ec1f11323f9f80df3733ead86448639c5051
enchan <enchant0130@gmail.com>
2025-03-22 22:49:04 +0900

    feat: #46 フロントエンドの環境変数定義を整理した
    - auth_domainとauth_audienceは抜かれても問題ないのでgit管理下に移動
    - 各環境変数にコメントを追加

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,9 +1,16 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  /** バックエンドのURL (CORSのAllow-Originに使用) */
   readonly VITE_BACKEND_URL: string
+
+  /** 認証エンドポイントのドメイン */
   readonly VITE_AUTH_DOMAIN: string
+
+  /** OAuth2.0 クライアントID */
   readonly VITE_AUTH_CLIENT_ID: string
+
+  /** JWTに載るaudの値 */
   readonly VITE_AUTH_AUDIENCE: string
 }
 
