e8650839094c08ded92058f2f9cd0b3b6bf70ad3
enchan <enchant0130@gmail.com>
2025-03-31 11:20:11 +0900

    feat: インフラ層にユーザリポジトリの実装を追加, インタフェースを少し修正

--- backend/src/domain/repositories/userRepository.ts
+++ backend/src/domain/repositories/userRepository.ts
@@ -1,7 +1,7 @@
 import type { User } from '../entities/user'
 
 export interface UserRepository {
-  getUser(id: User['id']): Promise<User | undefined>
-  insertUser(newUser: User): Promise<User>
+  getUserById(id: User['id']): Promise<User | undefined>
+  saveUser(newUser: User): Promise<User>
   // 現時点ではlist, deleteは不要
 }
---
+++ backend/src/infrastructure/repositories/userRepository.test.ts
@@ -0,0 +1,56 @@
+import { env } from 'cloudflare:test'
+import { ulid } from 'ulid'
+
+import type { User } from '@/domain/entities/user'
+import type { UserRepository } from '@/domain/repositories/userRepository'
+
+import { useUserRepositoryD1 } from './userRepository'
+
+describe('単一項目のCRUD', () => {
+  let repo: UserRepository
+
+  beforeAll(() => {
+    repo = useUserRepositoryD1(env.D1)
+  })
+
+  test('項目を作成できること', async () => {
+    const user: User = {
+      id: ulid(),
+      name: 'test-user',
+      auth0_user_id: 'auth0|0123456789',
+    }
+    const inserted = await repo.saveUser(user)
+    expect(user).toStrictEqual(inserted)
+  })
+
+  test('IDを指定して項目を取得できること', async () => {
+    const user: User = {
+      id: ulid(),
+      name: 'test-user',
+      auth0_user_id: 'auth0|0123456789',
+    }
+    const { id } = await repo.saveUser(user)
+
+    const stored = await repo.getUserById(id)
+    expect(stored).toBeDefined()
+  })
+
+  // TODO: Auth0のsub経由でも取得できるか確認する
+
+  test('挿入した項目を更新できること', async () => {
+    const user: User = {
+      id: ulid(),
+      name: 'test-user',
+      auth0_user_id: 'auth0|0123456789',
+    }
+    const stored = await repo.saveUser(user)
+
+    const input: User = {
+      ...stored,
+      name: 'updated',
+      auth0_user_id: 'auth0|9876543210',
+    }
+    const updated = await repo.saveUser(input)
+    expect(updated).toStrictEqual(input)
+  })
+})
