4577fe3ff5d839759fa876b0f69aa747248394e5
enchan <enchant0130@gmail.com>
2025-04-01 18:21:36 +0900

    feat: #71 ユーザ取得ユースケースを実装

--- backend/src/presentations/middlewares/auth/user.ts
+++ backend/src/presentations/middlewares/auth/user.ts
@@ -1,22 +1,10 @@
 import { createMiddleware } from 'hono/factory'
-import { ulid } from 'ulid'
-import { z } from 'zod'
 
 import type { User } from '@/domain/entities/user'
 import { useUserRepositoryD1 } from '@/infrastructure/repositories/userRepository'
+import { useUserUsecase } from '@/usecases/userUsecase'
 
 import type { Auth0JWTPayload } from './jwk'
-
-const Auth0UserInfoSchema = z.object({
-  sub: z.string(),
-  nickname: z.string(),
-  name: z.string(),
-  email: z.string(),
-  picture: z.string(),
-  updated_at: z.string(),
-  email_verified: z.boolean(),
-})
-type Auth0UserInfo = z.infer<typeof Auth0UserInfoSchema>
 
 export const userMiddleware = createMiddleware<{
   Bindings: Env
@@ -25,45 +13,24 @@ export const userMiddleware = createMiddleware<{
     user: User
   }
 }>(async (c, next) => {
-  const token = c.get('jwtPayload')
+  const { sub } = c.get('jwtPayload')
   const repo = useUserRepositoryD1(c.env.D1)
+  const usecase = useUserUsecase(repo)
 
-  const user = await repo.getUserByAuth0Id(token.sub)
-
-  if (user !== undefined) {
-    c.set('user', user)
+  const storedUser = await usecase.lookupUserByAuth0Id(sub)
+  if (storedUser !== undefined) {
+    c.set('user', storedUser)
     await next()
     return
   }
 
-  const authToken = c.req.header('Authorization') ?? ''
-  const userInfo = await fetchUserInfo(authToken)
-  if (userInfo === undefined) {
-    console.log('No user info found')
-    return c.text('Unauthorized', 401)
-  }
+  const token = c.req.header('Authorization') ?? ''
+  const newUser = await usecase.createTentativeUser({
+    authDomain: c.env.AUTH_DOMAIN,
+    token,
+  })
+  console.log(`New user registered. ${JSON.stringify(newUser)}`)
 
-  const newUser: User = {
-    id: ulid(),
-    name: userInfo.nickname,
-    auth0_user_id: userInfo.sub,
-  }
-  await repo.saveUser(newUser)
-  console.log(`new user ${newUser.name}(${newUser.id}) registered.`)
   c.set('user', newUser)
-
   await next()
 })
-
-const fetchUserInfo = async (authorizationToken: string):
-Promise<Auth0UserInfo | undefined> => {
-  const response = await fetch('https://enchan.jp.auth0.com/userinfo', { headers: { Authorization: authorizationToken } })
-  const responseJson = await response.json()
-  console.log(responseJson)
-
-  const parseResult = Auth0UserInfoSchema.safeParse(responseJson)
-  if (!parseResult.success) {
-    return undefined
-  }
-  return parseResult.data
-}
