a48e3b1f9a8b07f430f4cbae5fb877e5bfe7046a
enchan <enchant0130@gmail.com>
2025-04-09 22:59:52 +0900

    feat: クエリ片の生成処理を分割

--- backend/src/logic/queryBuilder/d1.ts
+++ backend/src/logic/queryBuilder/d1.ts
@@ -4,18 +4,104 @@ import { Query } from '.'
 import type { ConditionLeaf, ConditionNode } from './conditionTree'
 import { isLeaf } from './conditionTree'
 
+type ConditionParameters<T extends z.AnyZodObject> = ConditionLeaf<T, keyof T['shape']>['value'][]
+
+type QueryBuildResult<T extends z.AnyZodObject> = {
+  query: string
+  index: number
+  params: ConditionParameters<T>
+}
+
 export class D1Query<T extends z.AnyZodObject> extends Query<T> {
-  build(d1Database: D1Database, tableName: string): D1PreparedStatement {
+  private buildBaseQuery(index: number = 1, tableName: string):
+  QueryBuildResult<T> {
     const modelShape = this.modelSchema.shape as Record<string, unknown>
     const columns: (keyof T['shape'])[] = Object.keys(modelShape)
 
-    const baseQuery = [
+    const query = [
       'SELECT',
       columns.join(','),
       'FROM',
       tableName,
     ].join(' ')
 
+    return {
+      query,
+      index,
+      params: [],
+    }
+  }
+
+  private buildConditionQuery(index: number = 1): QueryBuildResult<T> {
+    const conditionNode = this.elementConditionNode
+    if (conditionNode === undefined) {
+      return {
+        query: '',
+        index,
+        params: [],
+      }
+    }
+
+    const { query, index: nextIndex } = buildExpression(conditionNode, index)
+    const params = buildConditionQueryParams(conditionNode)
+
+    return {
+      query,
+      index: nextIndex,
+      params,
+    }
+  }
+
+  private buildLimitQuery(index: number = 1): QueryBuildResult<T> {
+    const limit = this.elementLimit
+    if (limit === undefined) {
+      return {
+        query: '',
+        index,
+        params: [],
+      }
+    }
+    return {
+      query: `LIMIT ?${index}`,
+      index: index + 1,
+      params: [limit],
+    }
+  }
+
+  private buildOffsetQuery(index: number = 1): QueryBuildResult<T> {
+    const offset = this.elementOffset
+    if (offset === undefined) {
+      return {
+        query: '',
+        index,
+        params: [],
+      }
+    }
+    return {
+      query: `OFFSET ?${index}`,
+      index: index + 1,
+      params: [offset],
+    }
+  }
+
+  private buildOrderByQuery(index: number = 1): QueryBuildResult<T> {
+    const orderBy = this.elementOrder
+    if (orderBy === undefined) {
+      return {
+        query: '',
+        index,
+        params: [],
+      }
+    }
+
+    return {
+      query: `ORDER BY ${orderBy.key} ${orderBy.order.toUpperCase()}`,
+      index,
+      params: [],
+    }
+  }
+
+  build(d1Database: D1Database, tableName: string): D1PreparedStatement {
     // TODO: 要素(ORDERBYとかWHEREとか)ごとのクエリ組み立て(値は埋め込みでなくバインド)
     // TODO: WHERE句以外でもバインドしたい需要を踏まえると、indexはこのレイヤまで取り回すべきか?
 
@@ -30,7 +116,7 @@ export class D1Query<T extends z.AnyZodObject> extends Query<T> {
  */
 const buildConditionQueryParams = <T extends z.AnyZodObject>(
   node: ConditionNode<T>,
-): ConditionLeaf<T, keyof T['shape']>['value'][] => {
+): ConditionParameters<T>[] => {
   if (isLeaf(node)) {
     return [node.value]
   }
@@ -41,17 +127,6 @@ const buildConditionQueryParams = <T extends z.AnyZodObject>(
 
 /**
  * 条件式を組み立てる
- * @param node 条件ノード
- * @returns 組み立てられたクエリ
- */
-const buildConditionExpression = <T extends z.AnyZodObject>
-(node: ConditionNode<T>) => {
-  const { query } = buildExpression(node, 1)
-  return query
-}
-
-/**
- * 条件式を組み立てる (internal)
  * @param node 条件ノード
  * @param index プレースホルダインデックス
  * @returns 組み立てられたクエリと次のインデックス
