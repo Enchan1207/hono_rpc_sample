4e4d5d3b7b76f58c414a09868e1eda3681d3d47c
enchan <enchant0130@gmail.com>
2025-04-09 09:59:16 +0900

    feat: 条件式ビルダをd1に移動

--- backend/src/logic/queryBuilder/conditionTree.ts
+++ backend/src/logic/queryBuilder/conditionTree.ts
@@ -11,7 +11,7 @@ type ConditionCompositionNode<T extends z.AnyZodObject> =
 
 type Operator = '==' | '!=' | '<' | '>' | '>=' | '<='
 
-type ConditionLeaf<T extends z.AnyZodObject, K extends keyof T['shape']> = {
+export type ConditionLeaf<T extends z.AnyZodObject, K extends keyof T['shape']> = {
   key: K
   operator: Operator
   value: z.infer<T['shape'][K]>
@@ -21,7 +21,8 @@ export type ConditionNode<T extends z.AnyZodObject> =
   | ConditionLeaf<T, keyof T['shape']>
   | ConditionCompositionNode<T>
 
-const isLeaf = <T extends z.AnyZodObject>(node: ConditionNode<T>):
+/** 条件ノードがリーフかどうかを判断する */
+export const isLeaf = <T extends z.AnyZodObject>(node: ConditionNode<T>):
 node is ConditionLeaf<T, keyof T['shape']> =>
   (node as { type: unknown }).type === undefined
 
@@ -49,68 +50,3 @@ ConditionNodeEvery<T> => ({
   type: 'every',
   items,
 })
-
-/**
- * 条件クエリに必要なパラメータの配列を構成する
- * @param node 条件ノード
- * @returns パラメータの配列
- */
-export const buildConditionQueryParams = <T extends z.AnyZodObject>(
-  node: ConditionNode<T>,
-): ConditionLeaf<T, keyof T['shape']>['value'][] => {
-  if (isLeaf(node)) {
-    return [node.value]
-  }
-  else {
-    return node.items.map(buildConditionQueryParams).flat()
-  }
-}
-
-/**
- * 条件クエリを組み立てる
- * @param node 条件ノード
- * @returns 組み立てられたクエリ
- */
-export const buildConditionQuery = <T extends z.AnyZodObject>
-(node: ConditionNode<T>) => {
-  const { query } = buildQuery(node, 1)
-  return query
-}
-
-/**
- * 条件クエリを組み立てる (internal)
- * @param node 条件ノード
- * @param index プレースホルダインデックス
- * @returns 組み立てられたクエリと次のインデックス
- */
-const buildQuery = <T extends z.AnyZodObject>(
-  node: ConditionNode<T>,
-  index: number,
-): {
-  query: string
-  index: number
-} => {
-  if (isLeaf(node)) {
-    const query = ['(', node.key, node.operator, `?${index}`, ')'].join(' ')
-    return {
-      query,
-      index: index + 1,
-    }
-  }
-  else {
-    let currentIndex = index
-    const queries: string[] = []
-    for (const childNode of node.items) {
-      const result = buildQuery(childNode, currentIndex)
-      currentIndex = result.index
-      queries.push(result.query)
-    }
-
-    const children = queries.join(node.type === 'every' ? ' AND ' : ' OR ')
-    const query = ['(', children, ')'].join(' ')
-    return {
-      query,
-      index,
-    }
-  }
-}
--- backend/src/logic/queryBuilder/d1.ts
+++ backend/src/logic/queryBuilder/d1.ts
@@ -1,16 +1,89 @@
-import { Query } from '.'
-import { buildConditionQuery, buildConditionQueryParams } from './conditionTree'
+import type { z } from 'zod'
 
-export class D1Query<T> extends Query<T> {
+import { Query } from '.'
+import type { ConditionLeaf, ConditionNode } from './conditionTree'
+import { isLeaf } from './conditionTree'
+
+export class D1Query<T extends z.AnyZodObject> extends Query<T> {
   build(d1Database: D1Database, tableName: string): D1PreparedStatement {
-    const node = this.elementConditionNode
-    if (node === undefined) {
-      return
+    const modelShape = this.modelSchema.shape as Record<string, unknown>
+    const columns: (keyof T['shape'])[] = Object.keys(modelShape)
+
+    const baseQuery = [
+      'SELECT',
+      columns.join(','),
+      'FROM',
+      tableName,
+    ].join(' ')
+
+    // TODO: 要素(ORDERBYとかWHEREとか)ごとのクエリ組み立て(値は埋め込みでなくバインド)
+    // TODO: WHERE句以外でもバインドしたい需要を踏まえると、indexはこのレイヤまで取り回すべきか?
+
+    return d1Database.prepare(baseQuery).bind(params)
+  }
+}
+
+/**
+ * 条件クエリに必要なパラメータの配列を構成する
+ * @param node 条件ノード
+ * @returns パラメータの配列
+ */
+const buildConditionQueryParams = <T extends z.AnyZodObject>(
+  node: ConditionNode<T>,
+): ConditionLeaf<T, keyof T['shape']>['value'][] => {
+  if (isLeaf(node)) {
+    return [node.value]
+  }
+  else {
+    return node.items.map(buildConditionQueryParams).flat()
+  }
+}
+
+/**
+ * 条件式を組み立てる
+ * @param node 条件ノード
+ * @returns 組み立てられたクエリ
+ */
+const buildConditionExpression = <T extends z.AnyZodObject>
+(node: ConditionNode<T>) => {
+  const { query } = buildExpression(node, 1)
+  return query
+}
+
+/**
+ * 条件式を組み立てる (internal)
+ * @param node 条件ノード
+ * @param index プレースホルダインデックス
+ * @returns 組み立てられたクエリと次のインデックス
+ */
+const buildExpression = <T extends z.AnyZodObject>(
+  node: ConditionNode<T>,
+  index: number,
+): {
+  query: string
+  index: number
+} => {
+  if (isLeaf(node)) {
+    const query = ['(', node.key, node.operator, `?${index}`, ')'].join(' ')
+    return {
+      query,
+      index: index + 1,
+    }
+  }
+  else {
+    let currentIndex = index
+    const queries: string[] = []
+    for (const childNode of node.items) {
+      const result = buildExpression(childNode, currentIndex)
+      currentIndex = result.index
+      queries.push(result.query)
     }
 
-    const query = buildConditionQuery(node)
-    const params = buildConditionQueryParams(node)
-
-    return d1Database.prepare(query).bind(params)
+    const children = queries.join(node.type === 'every' ? ' AND ' : ' OR ')
+    const query = ['(', children, ')'].join(' ')
+    return {
+      query,
+      index,
+    }
   }
 }
